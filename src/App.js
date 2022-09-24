c183a51a99c6e38083c5f396611c271134951462
Lucas Lacerda Buettel <buettellucas@gmail.com>
2022-09-24 20:15:58 -0300

    versão sorteia palavra e ativa teclado

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import palavras from "./palavras"
 import "./Style/reset.css"
 import "./Style/style.css"
 import forca0 from "./assets/forca0.png"
@@ -9,25 +10,57 @@ import forca4 from "./assets/forca4.png"
 import forca5 from "./assets/forca5.png"
 import forca6 from "./assets/forca6.png"
 
- function Letras(props) {
-    console.log(props);
-    return (
-        <div className="letra"><a>{props.letra}</a></div>
-   )
+function conferePalavra(props){
+    const letraRecebida = props.target.innerText
+    console.log(letraRecebida.toLowerCase());
+
 }
+function Letras(props) {
+    if (props.novaPalavra === '') {
+        console.log(props.novaPalavra)
+        return (
+            <div className="letra-desabilitada"><a>{props.letra}</a></div>
+        )
+    } else {
+        return (
+            <div className="letra-habilitada" onClick={(a) => conferePalavra(a)}><a>{props.letra}</a></div>
+        )
+    }
+
+}
+
+
 export default function App() {
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const [novaPalavra, setNovaPalavra] = React.useState("")
+
+    function sorteiaPalavra() {
+        let palavraEscondida = "";
+        const indexSorteado = Math.floor(Math.random() * palavras.length);
+        const palavraSorteada = palavras[indexSorteado];
+        console.log(palavras[indexSorteado].length);
+
+        for (let i = 1; i <= palavraSorteada.length; i++) {
+            palavraEscondida = palavraEscondida + "_ "
+            console.log(palavraEscondida);
+        }
+
+        setNovaPalavra(palavraEscondida);
+
+    }
+
     return (<>
         <div className="container">
             <div className="topo">
                 <div className="forca">
                     <img src={forca0} />
                 </div>
-                <div className="botao"><p className="escolha">Escolher Palavra</p></div>
+                <div className="botao" onClick={sorteiaPalavra}><p className="escolha">Sortear Palavra</p></div>
+                <div className="setPalavra">{novaPalavra}</div>
             </div>
             <div className="baixo">
                 <div className="teclado">
-                    {alfabeto.map((a) => <Letras letra ={a.toUpperCase()}/>)}
+                    {alfabeto.map((a) => <Letras letra={a.toUpperCase()} novaPalavra={novaPalavra} />)}
                 </div>
                 <div className="chute"><h1>Já sei a palavra!</h1><input></input> <div><a className="button">Chutar</a></div></div>
             </div>
